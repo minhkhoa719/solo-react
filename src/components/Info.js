b0734489e0032c9c46e0aa660d5a7f1ae702bb8e
minhkhoa719 <xxminhkhoaxx147@gmail.com>
2022-08-08 16:21:04 +0700

    add icon button in Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { FontAwesomeIcon } from"@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
 export default function Info() {
     return (
         <div className="info">
@@ -7,8 +9,14 @@ export default function Info() {
             <h2 className="info-name">Laura Smith</h2>
             <h3 className="info-career">Frontend Developer</h3>
             <h4 className="info-weblink">laurasmith.website</h4>
-            <button type="button" className="btn-email"><img className="img-btn-email" src="images/icons8-mail-30.png"/> Email</button>
-            <button type="button" className="btn-linkedln">Linkedln</button>
+            <button type="button" className="btn-email">
+                <FontAwesomeIcon  icon={faEnvelope } />
+                <span style = {{"margin-left" : 8}}>Email</span>     
+            </button>
+            <button type="button" className="btn-linkedln">
+                <FontAwesomeIcon  icon={faLinkedin } />
+                <span style={{"margin-left" : 8}}>Linkedin</span>
+            </button>
         </div>
     )
 }
